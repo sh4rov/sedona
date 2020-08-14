2ac8b12fa846abc367fbdca8dce9809ff639bfb7
Anatoly Sharov <sh4rov@gmail.com>
2020-08-14 18:24:37 +0300

    add task styles

--- gulpfile.babel.js/tasks/styles.js
+++ gulpfile.babel.js/tasks/styles.js
@@ -9,12 +9,15 @@ import {
 import sass from 'gulp-sass'
 import browserSync from 'browser-sync'
 import plumber from 'gulp-plumber'
-import cleanCSS from 'gulp-clean-css'
 import rename from 'gulp-rename'
 import changed from 'gulp-changed'
 import groupMedia from 'gulp-group-css-media-queries'
+import postcss from 'gulp-postcss'
 import debug from 'gulp-debug'
-// import webpCSS from 'gulp-webpcss'
+import webpcss from 'webpcss'
+let plugins = [
+  webpcss()
+];
 
 export const styles = () =>
   src(paths.styles.src, {
@@ -26,10 +29,9 @@ export const styles = () =>
     extention: '.css'
   }))
   .pipe(sass())
-  // .pipe(webpCSS())
+  .pipe(postcss(plugins))
   .pipe(groupMedia())
   .pipe(dest(paths.styles.dest))
-  .pipe(cleanCSS())
   .pipe(rename({
     suffix: '.min'
   }))
